77672c4ddb4a6604523e33b3c32e2807a8601df4
Arub bin Ali <arubbinali@outlook.com>
2024-09-06 08:49:41 +0300

    `

--- sorting/bubble sort.py
+++ sorting/bubble sort.py
@@ -4,5 +4,5 @@ def bubble_sort(numbers):
         for __ in range(len(numbers) - _ - 1):
             if numbers[__] > numbers[__ + 1]:
                 numbers[__], numbers[__ + 1] = numbers[__ + 1], numbers[__]
-
+    
     return numbers
