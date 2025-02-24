91a31fd66ea33dd9b814c7bbaeb530be7825f961
arub <arubbinali@outlook.com>
2025-02-24 09:59:47 +0300

    `

--- Data structures & Algorithms/Python/sorting/insertion sort.py
+++ Data structures & Algorithms/Python/sorting/insertion sort.py
@@ -1,6 +1,6 @@
 #insertion sort
 def insertion_sort(numbers):
-    for _ in range(1, len(range(numbers))):
+    for _ in range(1, range(len(numbers))):
         number = numbers[_]
         index_of_prev_number = _ - 1
         while index_of_prev_number >= 0 and number < numbers[index_of_prev_number]:
